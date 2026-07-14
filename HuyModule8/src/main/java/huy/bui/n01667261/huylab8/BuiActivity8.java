4a89bb71cf9300335ceef2736cece3c584647e2e
HuyBui7261 <huyb85mmiv@gmail.com>
2026-07-14 03:43:37 -0400

    chagning the name of some files and adding all the fragments needed for the app

---
+++ HuyModule8/src/main/java/huy/bui/n01667261/huylab8/BuiActivity8.java
@@ -0,0 +1,14 @@
+package huy.bui.n01667261.huylab8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BuiActivity8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
